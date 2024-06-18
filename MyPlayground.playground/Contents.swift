009d84ad7fe865f4607a7583b8e34d9eac595961
shachar <leo@gmail.com>
2024-06-18 13:58:47 +0800

    final merge

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -5,15 +5,17 @@
  a. git status\
  查看當前目錄的狀態\
  b. git diff\
-
+ 比較當前的修改紀錄(與git add之前的檔案做對比)
  c. git add\
  把內容加到暫存區
  d. git reset\
+ 把資料回復到某個節點\
  e. git commit\
  把暫存區的資料commit(提交)到Repository(儲存庫)
  f. git log\
  查看git log(紀錄)
  g. git branch\
+ 查看目前有哪些分支
  h. git merge\
  i. git push [ repo_name ] [ branch_name ]\
  j. git remote\
@@ -23,6 +25,4 @@
  n. (Advanced) git reflog\
  o. (Advanced) git tag\
 
- use for look diff
- 
  */
