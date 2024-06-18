61d5b1536b2967e38753989a1052040c9c6bba31
shachar <leo@gmail.com>
2024-06-18 12:33:45 +0800

    first

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+/*:
+ 
+# 2. Here are some Git and GitHub commands we usually use in software development.Please explain the meanings and use cases of them.
+    
+ a. git status\
+ 查看當前目錄的狀態\
+ b. git diff\
+
+ c. git add\
+ 把內容加到暫存區
+ d. git reset\
+ e. git commit\
+ 把暫存區的資料commit(提交)到Repository(儲存庫)
+ f. git log\
+ 查看git log(紀錄)
+ g. git branch\
+ h. git merge\
+ i. git push [ repo_name ] [ branch_name ]\
+ j. git remote\
+ k. fork\
+ l. (Advanced) git rebase\
+ m. (Advanced) git cherry-pick\
+ n. (Advanced) git reflog\
+ o. (Advanced) git tag\
+        
+ 
+ 
+ */
